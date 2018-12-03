a3bb2b2fe534816978791a69caebe2c4c9835e93
Ashish Sharma <asharma380@sapient.com>
2018-12-03 12:55:49 +0530

    test2

--- up.sh
+++ up.sh
@@ -5,8 +5,8 @@ vagrant init concourse/lite && vagrant up
 sleep 60
 cd ..
 echo "### Setting up pipeline"
-fly -t lite login --concourse-url http://192.168.100.4:8080
+fly -t lite login --concourse-url http://ec2-13-233-175-12.ap-south-1.compute.amazonaws.com:8080
 fly -t tutorial sync
 yes | fly -t lite set-pipeline -p concourse-gradle-spring-boot -c concourse/pipeline.yml
 fly -t lite unpause-pipeline -p concourse-gradle-spring-boot
-echo "### Finished. Concourse available on: http://192.168.100.4:8080/"
+echo "### Finished. Concourse available on: http://ec2-13-233-175-12.ap-south-1.compute.amazonaws.com:8080/"
